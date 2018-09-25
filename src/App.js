0d34c48776756a970af2d36c7a21fd7a7570d452
Byteme <shvartsmand@gmail.com>
2018-09-24 20:49:25 -0700

    save

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Landing from "./pages/Landing";
+import Main from "./pages/Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+const App = () =>
+  
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/main" component={Main} />
+      <Footer />
+    </div>
+  </Router>
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = props => (
+    <h1>Navbar</h1>
+);
+
+export default Navbar;
